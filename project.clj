59101b507b2be2870f7010972ad5053f73750e64
Alex Ott <alexott@gmail.com>
2012-04-19 08:08:26 +0200

    update to clojure 1.3 & get rid of clojure-contrib

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject clj-serializer "0.1.1"
+(defproject clj-serializer "0.1.3"
   :description "Fast binary serialization and deserialization for Clojure data structures."
   :url "http://github.com/mmcgrana/clj-serializer"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-fork "true"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[clj-json "0.2.0"]
-                     [lein-clojars "0.5.0"]
-                     [org.clojars.mmcgrana/lein-javac "0.1.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[clj-json "0.5.0"]
+                     [lein-clojars "0.8.0"]
+                     ])
